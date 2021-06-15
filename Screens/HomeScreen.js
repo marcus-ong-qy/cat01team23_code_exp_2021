6e334d5b26678fe3e81c2d4329c352bc7883d3e9
HP <marcus.ong.qy@gmail.com>
2021-06-15 15:50:51 +0800

    marcus dat 2 afternoon

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useEffect, useState } from "react";
 import { Text, View, StyleSheet, Button } from 'react-native';
 import Constants from 'expo-constants';
 import { NavigationContainer } from '@react-navigation/native';
@@ -6,30 +7,135 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { FontAwesome } from "@expo/vector-icons";
 import { Card } from 'react-native-paper';
 
+import { nominalList } from '../components/NominalList.js' // explore using json or a database
+
+
 
 export default function HomeScreen({ userID }) {
-  console.log(userID)
+
+  function timeNow() {
+    var myDate = new Date();
+
+    let daysList = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    let monthsList = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Aug', 'Oct', 'Nov', 'Dec'];
+
+    let date = myDate.getDate();
+    let month = monthsList[myDate.getMonth()];
+    let year = myDate.getFullYear();
+    let day = daysList[myDate.getDay()];
+
+    let today = `${date} ${month} ${year}, ${day}`;
+
+    let amOrPm;
+    let twelveHours = function (){
+      if(myDate.getHours() > 12) {
+          amOrPm = 'PM';
+          let twentyFourHourTime = myDate.getHours();
+          let conversion = twentyFourHourTime - 12;
+          return `${conversion}`
+
+      } else if(myDate.getHours() == 12) {
+          amOrPm = 'PM';
+          return `${myDate.getHours()}` 
+
+      } else if(myDate.getHours() == 0) {
+          amOrPm = 'AM';
+          return `${myDate.getHours()}` 
+                  
+      } else {
+          amOrPm = 'AM';
+          return `${myDate.getHours()}`}
+    };
+
+    let hours = twelveHours() < 10 ? `0${twelveHours()}`: twelveHours() ;
+    let minutes = myDate.getMinutes() < 10 ? `0${myDate.getMinutes()}`: myDate.getMinutes() ;
+
+    let currentTime = `${hours}:${minutes} ${amOrPm}`;
+
+    return `${today} ${currentTime}`
+  }
+
+  const idFound = nominalList[userID]? true:false
+
+  const [isInOffice, setIsInOffice] = useState(idFound? nominalList[userID]['isInOffice']:false);
+
+  function checkInButton() {
+    if (!nominalList[userID]['wfh'])
+    return (
+      <Button 
+        title='Check-in to Workplace' 
+        onPress={ () => { 
+          nominalList[userID]['isInOffice'] = !nominalList[userID]['isInOffice'];
+          setIsInOffice(!isInOffice)
+        }}
+      />
+    )
+  }
+
+  var currentdate = timeNow()  //todo make this self updating
+
   return (
     <View style={styles.container}>
-      <Text style={styles.paragraph}>
-        User ID = { userID }
-      </Text>
+      <Card>
+        <Text style={styles.dateTime}> { currentdate } </Text>
+
+        <Text style={styles.idName}> { idFound? nominalList[userID]['id']: 'nil' }: { idFound? nominalList[userID]['name']: 'nil' } </Text>
+
+        <Text style={styles.schedule}> Schedule: { idFound? nominalList[userID]['wfh']? 'Work From Home':'Report To Office' : 'nil' } </Text>
+
+        <Text style={styles.paragraph}> Status: </Text>
+        <Text style={styles.status}> { idFound? isInOffice? 'OFFICE':'HOME' : 'nil' } </Text>
+        
+        <Text style={styles.cohort}> Cohort: { idFound? nominalList[userID]['cohort']: 'nil' } </Text>
+      </Card>
+
+      {checkInButton()}
+
     </View>
   )
 }
 
 const styles = StyleSheet.create({
+  dateTime: {
+    margin: 12,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  idName: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  paragraph: {
+    margin: 12,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  status: {
+    // margin: 24,
+    fontSize: 48,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  schedule: {
+    margin: 12,
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  cohort: {
+    margin: 12,
+    fontSize: 22,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
     paddingTop: Constants.statusBarHeight,
     backgroundColor: '#ecf0f1',
     padding: 8,
-  },
-  paragraph: {
-    margin: 24,
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
   },
 });
--- components/NominalList.js
+++ components/NominalList.js
@@ -1,10 +1,44 @@
-var nominalList = [
-    {
-      "name": "bob",
-      "id": "001",
-      "cohort": "A",
-      "wfh": true,
-      "isInOffice": false,
-      "exception": false
-    }
-]
+var nominalList = {
+  "001":{
+    "name": "bob1",
+    "id": "001",
+    "cohort": "A",
+    "wfh": true,
+    "isInOffice": false,
+    "exception": false
+  },
+  "002":{
+    "name": "bob2",
+    "id": "002",
+    "cohort": "A",
+    "wfh": true,
+    "isInOffice": false,
+    "exception": false
+  },
+  "003":{
+    "name": "bob3",
+    "id": "003",
+    "cohort": "A",
+    "wfh": false,
+    "isInOffice": true,
+    "exception": false
+  },
+  "004":{
+    "name": "bob4",
+    "id": "004",
+    "cohort": "A",
+    "wfh": false,
+    "isInOffice": false,
+    "exception": false
+  },
+  "005":{
+    "name": "bob5",
+    "id": "005",
+    "cohort": "A",
+    "wfh": false,
+    "isInOffice": false,
+    "exception": false
+  },
+}
+
+export { nominalList }
