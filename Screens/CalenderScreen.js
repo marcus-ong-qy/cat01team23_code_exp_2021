5a599c18e00babe9d3206e422ccbf0f8272f2090
HP <marcus.ong.qy@gmail.com>
2021-06-16 12:11:05 +0800

    yes

--- Screens/CalenderScreen.js
+++ Screens/CalenderScreen.js
@@ -1,5 +1,6 @@
 
-import React, { useState , useEffect} from "react";
+import React, { useState , useEffect} from 'react'
+import DatePicker from 'react-native-date-picker'
 import {
   FlatList,
   StyleSheet,
@@ -7,92 +8,122 @@ import {
   View,
   Button,
   TouchableOpacity,
+  Platform
 } from "react-native";
-import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
-import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
-import DropDownPicker from 'react-native-dropdown-picker';
+import Constants from 'expo-constants';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { Ionicons } from "@expo/vector-icons";
+import firebase from "../database/firebaseDB";
 
-function CalenderSelect({navigation}) {
+const db = firebase.firestore().collection("timetable");
+
+async function loadAppointmentData() {
+  setLoad(true)
+
+
+  setLoad(false)
+  setDisplayTime(busData.next.time)
+  console.log(busData.next.time)
+  }
+
+export default function CalenderScreen() {
+  //firebase
+  const [appointmentDate, setAppointmentDate] = useState(new Date());
+  const [hasAppointment, setHasAppointment] = useState(0);
+  const [timetable, setTimetable] = useState([]);
+
+  
+  // Load Firebase data on start
+  //alt methods
+  useEffect(() => {
+    fetchTimetable();
+  }, [])
+
+  const fetchTimetable=async()=>{
+    const data=await db.get();
+    data.docs.forEach(item=>{
+     setTimetable([...timetable,item.data()])
+    })
+  }
+
+  function uploadData()
+  {
+    alert(timetable[0].appointmenHH)
+    const newNote = {
+      appointmenHH: date.getHours(),
+      appointmentDD: date.getDay(),
+      appointmentMM: date.getMonth(), 
+      appointmentMin: date.getMinutes(), 
+      userId: 1,
+    };
+    db.add(newNote);
+  }
+  //firebase end
+
+
+  //calendar settings
+  const [date, setDate] = useState(new Date());
+  const [mode, setMode] = useState('date');
+  const [show, setShow] = useState(false);
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+    setShow(Platform.OS === 'ios');
+    setDate(currentDate);
+    setAppointmentDate(date)
+    uploadData()
+  };
+
+  const showMode = (currentMode) => {
+    setShow(true);
+    setMode(currentMode);
+  };
+
+  const showDatepicker = () => {
+    showMode('date');
+  };
+
+  const showTimepicker = () => {
+    showMode('time');
+  };
+  //calendar settings end
+
+
   return (
     <View style={styles.container}>
-        <Calendar
-  
-  // Handler which gets executed on day press. Default = undefined
-  onDayPress={(day) => {navigation.navigate("Timetable", {day});}}
-  // Handler which gets executed on day long press. Default = undefined
-  onDayLongPress={(day) => {navigation.navigate("Timetable", {day});}}
-  // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
-  monthFormat={'MMMM'}
-  // Handler which gets executed when visible month changes in calendar. Default = undefined
-  disableMonthChange={true}
-  disableArrowLeft={true}
-  // Disable right arrow. Default = false
-  disableArrowRight={true}
-  enableSwipeMonths={false}
-
-  markingType={'period'}
-  markedDates={{
-    '2021-06-16': {startingDay: true, color: '#50cebb', textColor: 'white'},
-    '2021-06-17': {color: '#70d7c7', textColor: 'white'},
-    '2021-06-18': {color: '#70d7c7', textColor: 'white', marked: true, dotColor: 'white'},
-    '2021-06-19': {color: '#70d7c7', textColor: 'white'},
-    '2021-06-20': {endingDay: true, color: '#50cebb', textColor: 'white'},
-  }}
-/>
-
-<TouchableOpacity
-        onPress={() => {
-          navigation.navigate("Timetable", {});
-        }}
-      >
-      <Text> Test</Text>
-      </TouchableOpacity>
-    </View>
-  )
-}
-
-function TimetableSelect({route}) {
-  const {year, month, day, timestamp, dateString} = route.params
-
-
-
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <Button onPress = {() => console.log('day', day["day"])}>
-    test
-    </Button>
-
-    <DropDownPicker
-    items={[
-        {label: 'Item 1', value: 'item1'},
-        {label: 'Item 2', value: 'item2'},
-    ]}
-    defaultIndex={0}
-    containerStyle={{height: 40}}
-    onChangeItem={item => console.log(item.label, item.value)}
-/>
+      <Text>{hasAppointment? appointmentDate.toString() : ""}</Text>
+      <Text>{hasAppointment? "Appointment good to go": "Looks like you have No Appointment booked, "}</Text>
+      <Text>{hasAppointment? " " : "We have automatically booked a slot for you, is it okay? "}</Text>
+      <Button onPress={showDatepicker} title="Customise Date" />
+      <Button onPress={showTimepicker} title="Customise Time" />
+      {show && (
+        <DateTimePicker
+          testID="dateTimePicker"
+          value={date}
+          mode={mode}
+          is24Hour={true}
+          display="default"
+          onChange={onChange}
+        />
+      )}
     </View>
   );
-}
+};
 
-const Stack = createStackNavigator();
-
-export default function CalenderScreen({navigation}) {
-  return (
-    <NavigationContainer independent = {true}>
-      <Stack.Navigator headerMode="none">
-        <Stack.Screen name="CalenderTable" component={CalenderSelect} />
-        <Stack.Screen name="Timetable" component={TimetableSelect} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#eee",
-    alignItems: "center",
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
